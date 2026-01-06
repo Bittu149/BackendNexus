ad65990999a7a162425a9e4a77867e694384720d
Bittu kumar <bittukumarab149@gmail.com>
2026-01-06 14:36:33 +0530

    Add time stamp

--- Mongoose/Day3/first.js
+++ Mongoose/Day3/first.js
@@ -1,11 +1,13 @@
 const bcrypt = require("bcrypt");
-
+//npm bcrypt
 const password = "Bittu@12345";
 
 // Hashcode + Salt
 
 async function Hashing(){
+console.time("hash");
 const hashpass =  await bcrypt.hash(password,10);
+console.timeEnd("hash");
 console.log(hashpass);
 }
 
