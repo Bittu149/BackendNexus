02c8b240e60ae7956fd95ec85383f6033427ad75
Bittu kumar <bittukumarab149@gmail.com>
2026-01-06 14:26:58 +0530

    How to store password using hashing

--- Mongoose/Day3/first.js
+++ Mongoose/Day3/first.js
@@ -1,1 +1,12 @@
-console.log("Hello coder Army");
+const bcrypt = require("bcrypt");
+
+const password = "Bittu@12345";
+
+// Hashcode + Salt
+
+async function Hashing(){
+const hashpass =  await bcrypt.hash(password,10);
+console.log(hashpass);
+}
+
+Hashing();
