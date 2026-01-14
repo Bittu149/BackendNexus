37a6c0e9949edfa4d364c4cb39fd2f3a7cac1924
Bittu kumar <bittukumarab149@gmail.com>
2026-01-14 11:14:00 +0530

    add more feature in logout

--- Mongoose/Day8/Routes/Auth.js
+++ Mongoose/Day8/Routes/Auth.js
@@ -51,7 +51,7 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", (req,res)=>{
     
   try{
-    res.cookie("Token","mkdjuhdjnwdj");
+    res.cookie("Token",null,{expires:new Date(Date.now())});
     res.send("Logout Sucessfully");
 
   }
