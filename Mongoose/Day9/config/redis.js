f8552171c10ec57b1e8c99575b7908a0ec532597
Bittu kumar <bittukumarab149@gmail.com>
2026-01-15 18:10:15 +0530

    Connected to redis

--- Mongoose/Day9/config/redis.js
+++ Mongoose/Day9/config/redis.js
@@ -1,4 +1,26 @@
-const redis = require ('redis');
+const redis = require('redis');
 
- redis.createClient();
- 
+const redisClient = redis.createClient({
+    username: 'default',
+    password: '4SZdo1dqTEFcpJWze39mJiXPAbdZb5sV',
+    socket: {
+        host: 'redis-14829.crce182.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 14829
+    }
+});
+
+redisClient.on('error', (err) => {
+    console.error("Redis Client Error:", err);
+});
+
+const connectRedis = async () => {
+    try {
+        await redisClient.connect();
+        console.log("Connected to Redis");
+    } catch (err) {
+        console.error("Failed to connect to Redis:", err.message);
+        throw err;
+    }
+};
+
+module.exports = { redisClient, connectRedis };
--- Mongoose/Day9/index.js
+++ Mongoose/Day9/index.js
@@ -5,38 +5,31 @@ const User = require("./user");
 const validateUser = require("./Validate");
 const bcrypt = require("bcrypt");
 const cookieParser = require('cookie-parser');
-const  jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const userAuth = require("./Middleware/userAuth");
 const env = require('dotenv').config({ path: __dirname + '/../.env' });
 const authRouter = require("./Routes/Auth");
 const userRouter = require("./Routes/user");
-
-
+const { redisClient, connectRedis } = require("./config/redis");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/auth", authRouter);
 app.use("/", userRouter);
-// npm cookie parser
-// npm install 
-// npm jwttoken
 
-// GET all users
-main()
-  .then(async () => {
-    console.log("Connected to DB (LOCAL)");
-    const port = process.env.PORT_NO || 3000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+// Initialize connection
+const InitializeConnection = async () => {
+  try {
+    await Promise.all([connectRedis(), main()]);
+    console.log("Connected to DB and Redis");
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listen at port ${process.env.PORT || 3000}`);
     });
+  } catch (err) {
+    console.log("Error: " + err.message);
+   // process.exit(1);
+  }
+};
 
-    // CORRECT FIND
-    const result = await User.find({ firstName: "Bittu" });
-    console.log("FOUND:", result);
-  })
-  .catch(err => console.log(err));
-
-  // interview question 
-  // why use routes
-  // hm isliye use karte hai ki code ki readability kaam ho jati tab 
-  // or team me kaam karte time bhi asani hoti hai 
+InitializeConnection();
