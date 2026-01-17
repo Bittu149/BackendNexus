8a90dd6f602785a956193efe9c693a967076f428
Bittu kumar <bittukumarab149@gmail.com>
2026-01-17 15:05:51 +0530

    final setup ratelimitter

--- Mongoose/Day10/Middleware/rateLimiter.js
+++ Mongoose/Day10/Middleware/rateLimiter.js
@@ -1,11 +1,21 @@
-const jwt = require('jsonwebtoken');
-const User = require('../user');
+
 const { redisClient } = require('../config/redis');
 
-const rateLimiter = async (req, res, next){
+const rateLimiter = async (req, res, next) => {
 
     try{
         const ip = req.ip;
+
+        const count = await redisClient.incr(ip);
+
+        if(count > 60){
+            throw new Error("User limit excedded");
+        }
+
+        if(count ==1){
+           await redisClient.expire(3600); // 1 hour
+        }
+        next();
 
     }
     catch(err){
