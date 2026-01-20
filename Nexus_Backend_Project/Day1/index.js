9b05c500cf019b70ec327b5bf87ca70b202f37c1
Bittu kumar <bittukumarab149@gmail.com>
2026-01-20 14:59:52 +0530

    update

--- Nexus_Backend_Project/Day1/index.js
+++ Nexus_Backend_Project/Day1/index.js
@@ -1,13 +1,26 @@
-const  { GoogleGenAI } = require ('@google/genai');
+const env = require("dotenv").config();
+const express = require('express');
+const app = express();
+const aichat = require('./Aichat');
 
-const ai = new GoogleGenAI({apiKey: "AIzaSyCoubggXsyJoWqlpEJeGY3cEbtevDkVrWU"});
+app.use(express.json()); // convert incoming json data to js object
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
+app.post('/chat', async (req, res) => {
+    const { msg } = req.body;
 
-await main();
+    const msgForAI = [
+        {
+            role: "user",
+            parts: [
+                { text: msg }
+            ]
+        }
+    ];
+
+    const ans = await aichat(msgForAI);
+    res.send(ans);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
