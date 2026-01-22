a2db46e4c5c1f2d5550453e7b4f07ba607c77b5d
Bittu kumar <bittukumarab149@gmail.com>
2026-01-22 11:55:59 +0530

    Today's final update

--- Nexus_Backend_Project/Day1/index.js
+++ Nexus_Backend_Project/Day1/index.js
@@ -5,24 +5,36 @@ const aichat = require('./Aichat');
 
 app.use(express.json()); // convert incoming json data to js object
 
-const chattingHistory = [];
+const chattingHistory = {};
 // we  will install our user chat history here
 // key value pair me history ko store karenge
+// key will be user id value will be array of messages
 
 
 app.post('/chat', async (req, res) => {
-    const { msg } = req.body;
+    const { msg, id } = req.body;
 
-    const msgForAI = [
-        {
-            role: "user",
-            parts: [
-                { text: msg }
-            ]
-        }
-    ];
+    if(!chattingHistory[id]){
+        chattingHistory[id] = [];
+    }
+ 
+    // extract user history
+    const History = chattingHistory[id];
 
-    const ans = await aichat(msgForAI);
+    // History+current ; array
+    const promptmessage = [...History ,{  // spread operator
+        role:'user',
+        parts:[{text:msg}]
+    }]
+
+
+
+    const ans = await aichat(promptmessage);
+
+    // User question ko bhi insert karna hai 
+    // model ke response ko bhi insert karna hai
+    History.push({role:'user', parts:[{text:msg}]});
+    History.push({role:'model', parts:[{text:ans}]});
     res.send(ans);
 });
 
