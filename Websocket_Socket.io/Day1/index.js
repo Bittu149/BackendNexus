1a1ba1857c03e9645b32dcd3d5fbb591c7ba6fe9
Bittu kumar <bittukumarab149@gmail.com>
2026-01-25 16:45:34 +0530

    Disconnect server

--- Websocket_Socket.io/Day1/index.js
+++ Websocket_Socket.io/Day1/index.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const app = express();
 const {Server} = require("socket.io");
+const http = require('http');
 
-app.listen(3000,()=>{
-    console.log("Server is listen at post no 3000");
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection",(socket)=>{
+
+    socket.on('message',(data)=>{// io matlab mai sabko listen kar raha hu multiple
+        io.emit('new-message',data);// emit matlab bhejna 
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("Disconnected from server")
+    })
+
 })
+
+server.listen(3000,()=>{
+    console.log("Server is listen at port no 3000");
+})
+
+
+
+
+
+// const server = app.listen(3000,()=>{
+//     console.log("Server is listen at post no 3000");
+// })
+
+
+
+// 3 way handshake protocol
+// 4 way handshake tcp tier down
+// long polling
+// HTTP streaming -> send data in stream form or chunck form...
+// Problem: Head-of-line Blocking-->
+// Tcp connection upgrade hota hai Websocket me ->2 way handshake 
+// aphle tcp connection banta hai then wo update hoke websocket hota hai
+// websocket ke uper build hua hai socket.io
+// socket.io q use kar rahe hai q ki bohut saari drawback hai websocket ke 
+// chat gpt pe dekh na hai 
